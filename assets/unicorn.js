a6d4a14b2882b09549c333db37f0e37750b32a35
Alex Hendershott <alex.hendershott@gmail.com>
2016-02-10 18:53:05 -0500

    Rearranged footer

--- assets/unicorn.js
+++ assets/unicorn.js
@@ -8,12 +8,20 @@ $( ".join" ).hover(
   }
 );
 
-$( ".eventblock" ).hover(
-  function() {
-    $( this ).addClass( "animated swing" );
-  }, function() {
-    $( this ).removeClass( "animated swing" );
-  }
-);
+// $( "#center" ).hover(
+//   function() {
+//     $( ".unicorn-head" ).addClass( "animated pulse" );
+//   }, function() {
+//     $( ".unicorn-head" ).removeClass( "animated pulse" );
+//   }
+// );
+
+// $( ".meetup-block" ).hover(
+//   function() {
+//     // $( ".badge").addClass( "animated bounceIn" );
+//   }, function() {
+//     // $( ".badge" ).removeClass( "animated bounceIn" );
+//   }
+// );
 
 });
